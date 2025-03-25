c60b6a79e861c0db9d090e0ea3cfac6fe040a916
Gerald Nyeo <geraldnyeo@gmail.com>
2025-03-25 21:52:32 +0800

    integration testing

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@ import { ref, onMounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import axios from 'axios';
 
+const API = process.env.API_URL || "http://localhost:3000/";
 const message = ref("");
 
 const getServerResponse = async () => {
-  const res = await axios.get("http://localhost:3000/");
+  const res = await axios.get(API);
   message.value = res.data;
 }
 
