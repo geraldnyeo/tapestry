c984c848a0489ea1370ae53d4458f1b493124988
Gerald Nyeo <geraldnyeo@gmail.com>
2025-03-25 21:38:42 +0800

    integration testing

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+import axios from 'axios';
+
+const message = ref("");
+
+const getServerResponse = async () => {
+  const res = await axios.get("http://localhost:3000/");
+  message.value = res.data;
+}
+
+onMounted(async () => {
+  await getServerResponse();
+})
+</script>
+
+<template>
+  <div>
+    <h1>MESSAGE FROM THE SERVER:</h1>
+    <p>{{ message }}</p>
+
+    <RouterView />
+  </div>
+</template>
